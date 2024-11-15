4b78704d2942f1c74cb14278a62593210a4e5eca
Steven Nguyen <zerga556@yahoo.com>
2024-11-15 01:02:51 -0600

    Imported font Orbitron
    Hero font changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,14 @@
 import { LenisProvider } from "./components/LenisProvider"
 import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
-import { Inter } from "next/font/google"
+import { Orbitron } from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const orbitron = Orbitron({
+  weight: ["700"],
+  subsets: ["latin"],
+  variable: "--font-tech",
+})
 
 export default function RootLayout({
   children,
@@ -14,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${orbitron.variable}`}>
         <LenisProvider>
           <Navbar />
           <main>{children}</main>
